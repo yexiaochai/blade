83ade9b003e1ec8060cd8e08d51093840bf0da0a
skyweaver213 <happyday0213@126.com>
2014-07-12 16:34:59 +0800

    update demo

--- demo/views/validate1.js
+++ demo/views/validate1.js
@@ -17,6 +17,7 @@ define(['View', getViewTemplatePath('validate1'), 'cValidate'], function (View, 
       //获取form下所有要验证的input
       var $inputs = $('.validate-form  .va-input');
       var num = [];  //验证是否重复数字
+      this.$('.error_info').detach(); // 清空所有气泡提示
       //遍历所有要验证的input
       for(var i = 0,len = $inputs.length;i < len;i++) {
         //取验证的类型
@@ -36,7 +37,14 @@ define(['View', getViewTemplatePath('validate1'), 'cValidate'], function (View, 
         if(isTrue) {
           console.log($inputs.eq(i).prev('label').html() + ' is true');
         } else {
-          console.error($inputs.eq(i).prev('label').html() + ' is false');
+          //创建错误提示气泡
+          var error_info = document.createElement('div');
+          error_info.innerHTML = $inputs.eq(i).prev('label').html() + '不正确';
+          error_info.style.cssText = 'position:absolute;top:'+($inputs.eq(i).position().top-10) + 'px;left:'+($inputs.eq(i).position().left+210)+'px;';
+          error_info.className = 'error_info';
+          $inputs.eq(i).after(error_info);
+        
+          console.log($inputs.eq(i).prev('label').html() + ' is false');
         }
       }
     },
