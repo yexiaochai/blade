f259fd6f08dd683eca02afcfe001ac06f673a8bb
yexiaochai <549265480@qq.com>
2014-07-16 17:32:01 +0800

    demo

--- demo/views/validate1.js
+++ demo/views/validate1.js
@@ -12,35 +12,35 @@ define(['View', getViewTemplatePath('validate1'), 'cValidate'], function (View, 
     demo1: function () {
       try {
         console.clear();  //清空console里的内容
-      } catch(e) {   }
+      } catch (e) { }
 
       //获取form下所有要验证的input
       var $inputs = $('.validate-form  .va-input');
       var num = [];  //验证是否重复数字
       this.$('.error_info').detach(); // 清空所有气泡提示
       //遍历所有要验证的input
-      for(var i = 0,len = $inputs.length;i < len;i++) {
+      for (var i = 0, len = $inputs.length; i < len; i++) {
         //取验证的类型
         var val_type = $inputs.eq(i).attr('val-type');
         //重复数字验证
-        if(val_type == 'isAllowSetTradingPass') {
+        if (val_type == 'isAllowSetTradingPass') {
           num[$inputs.eq(i).attr('num')] = $inputs.eq(i).val();
-          if(num.length==2) {
+          if (num.length == 2) {
             //console.log(num);
-            cValidate.isAllowSetTradingPass(num) ?  console.log('重复数字: is true') : console.error( '重复数字: is false');
+            cValidate.isAllowSetTradingPass(num) ? console.log('重复数字: is true') : console.error('重复数字: is false');
           }
           continue;
         }
         //验证input里的值是否符合该类型的验证条件
-        var isTrue =  cValidate[val_type]($inputs.eq(i).val());
+        var isTrue = cValidate[val_type]($inputs.eq(i).val());
         //输出验证信息
-        if(isTrue) {
+        if (isTrue) {
           console.log($inputs.eq(i).prev('label').html() + ' is true');
         } else {
           //创建错误提示气泡
           var error_info = document.createElement('div');
           error_info.innerHTML = $inputs.eq(i).prev('label').html() + '不正确';
-          error_info.style.cssText = 'position:absolute;top:'+($inputs.eq(i).position().top-10) + 'px;left:'+($inputs.eq(i).position().left+160)+'px;';
+          error_info.style.cssText = 'position:absolute;top:' + ($inputs.eq(i).position().top - 10) + 'px;left:' + ($inputs.eq(i).position().left + 160) + 'px;';
           error_info.className = 'error_info';
           $inputs.eq(i).after(error_info);
 
@@ -50,6 +50,8 @@ define(['View', getViewTemplatePath('validate1'), 'cValidate'], function (View, 
     },
 
     onPreShow: function () {
+
+
       this.turning();
     },
 
