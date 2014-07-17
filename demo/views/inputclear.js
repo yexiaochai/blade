891074e209ad03084a18a2d953ed2f16fbe3601f
skyweaver213 <happyday0213@126.com>
2014-07-18 00:26:17 +0800

    add input

--- demo/views/inputclear.js
+++ demo/views/inputclear.js
@@ -1,4 +1,4 @@
-define(['View', getViewTemplatePath('inputclear')], function (View, viewhtml) {
+define(['View', getViewTemplatePath('inputclear'), 'UIInputClear'], function (View, viewhtml, UIInputClear) {
 
   return _.inherit(View, {
     onCreate: function () {
@@ -6,14 +6,58 @@ define(['View', getViewTemplatePath('inputclear')], function (View, viewhtml) {
     },
 
     events: {
-      'click .demo1': 'demo1'
+      'focus .demo1': 'demo1Action',
+      'click .new-input': 'newInput'
     },
 
-    demo1: function () {
+    demo1Action: function () {
 
+    },
+
+    newInput: function() {
+      this.$('.parent-div').append('' +
+          '<div class="demo1div pos-rel input-div">' +
+          '<input type="text" class="demo1 d-input"  value=""/>' +
+          '</div>');
     },
 
     onPreShow: function () {
+      if(!this.inputClear1) {
+        this.inputClear1 = new UIInputClear({
+          datamodel: {
+            container: '.demo1',
+            offset: {
+
+            },
+            parent:'.parent-div',
+            clearClass: '.my-clear'
+
+          },
+          clearCallback: function() {
+            console.log('clearCallback');
+            alert('i am clearCallback');
+          }
+        });
+
+      }
+
+      if(!this.inputClear2) {
+        this.inputClear2 = new UIInputClear({
+          datamodel: {
+            container: '.demo2',
+            offset: {
+
+            }
+
+          },
+          clearCallback: function() {
+            console.log('页面加载后不需要再生成input');
+            alert('页面加载后不需要再生成input');
+          }
+        });
+
+      }
+
       this.turning();
     },
 
