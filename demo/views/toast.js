05e3b6236cba3e7b88d0a30d31100822bed02266
叶小钗 <549265480@qq.com>
2018-04-07 16:00:50 +0800

    Update toast.js

--- demo/views/toast.js
+++ demo/views/toast.js
@@ -71,7 +71,10 @@
 		if(i === 0) {
 			var infoWindow = new BMap.InfoWindow(content,opts);  // 创建信息窗口对象 
 			map.openInfoWindow(infoWindow,_p); //开启信息窗口
-		  marker.setAnimation(BMAP_ANIMATION_BOUNCE);
+			
+			var circle = new BMap.Circle(_p,1000,{fillColor:"blue", strokeWeight: 1 ,fillOpacity: 0.3, strokeOpacity: 0.3});
+    map.addOverlay(circle);
+		 
 		}
 	}
 	function addClickHandler(content,marker){
